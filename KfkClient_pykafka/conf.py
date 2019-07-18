c5bc3c6d8462e087f8aaf0fe8df3fd73cc645143
bojingqian <jqian_bo@163.com>
2019-07-18 14:38:42 +0800

    update dev

--- KfkClient_pykafka/conf.py
+++ KfkClient_pykafka/conf.py
@@ -3,7 +3,8 @@
 # @Author: JingQian Bo
 # @Create Time: 2018/9/27-下午3:48
 
-demo_Sever = '10.211.55.5:9092'
-demo_Key = b'spider_demo'
+# 10.211.55.5:9092
+demo_Sever = '0.0.0.0:9092'
+demo_Key = b'demo'
 demo_Topic = b'test'
 demo_GroupID = 'test2'
